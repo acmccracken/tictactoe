d701cc239dd2b905576c915378fe2661bf96282e
Alan McCracken <acmccracken@gmail.com>
2020-02-04 08:50:27 -0600

    updating

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ const test2 = [0, 0, 0];
 const test3 = [0, 0, 0];
 
 
-const row1 = [-1, 0, 0];
-const row2 = [1, -1, 1];
-const row3 = [1, -1, -1];
-const grid = [row1, row2, row3];
+const row1 = [0, 0, 0];
+const row2 = [0, 0, 0];
+const row3 = [0, 0, 0];
+const grid = [[row1],[row2], [row3]];
 //winnerWinner will = [row1, row2, row3]; after testing
-const winnerWinner = [test1, test2, test3];
+const winnerWinner = [[test1], [test2], [test3]];
 
 
 /*------Variables (state)------*/
@@ -43,10 +43,9 @@ function init(){
 }
 
 function clickBoard(){
-    console.log(event.target.id)
     idx = event.target.id;
     a = idx % 3;
-    if(idx === 0){
+    if(idx == 0){
         b = 0
     }else{
         b = idx / 3;
@@ -78,7 +77,7 @@ function winCondition(){
       }
       if( grid[j][i] > 0){
         winnerWinner[1][i] ++;
-      }else if(grid[j][i] < 0){
+        }else if(grid[j][i] < 0){
         winnerWinner[1][i] --;
       }      
 
@@ -97,7 +96,6 @@ function winCondition(){
 
     
   }
-  console.log(winnerWinner);
 
   for (i = 0; i < 3; i ++){
     for(j = 0; j < 3; j ++){
@@ -113,6 +111,10 @@ function winCondition(){
   }else if (theWinner < 0){
     console.log("O wins");
   }
+  
+        console.log(winnerWinner);
+        console.log(grid);
+    
 }
 
 function render(){
@@ -122,7 +124,10 @@ function render(){
     }else if(turn < 0){
         changeSquare.textContent = "O";
     }
+    grid[a][b] = turn;
     turn *= -1;
+    console.log(turn)
+    winCondition();
 
 
 }
